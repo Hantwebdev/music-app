68c4daa936e26c0852e07347ede50645c74cc3f4
Ha Nguyen <hant582betterdev.contact@gmail.com>
2024-05-01 22:07:42 +0700

    Trang bài hát - client

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("ok");
+import express, { Express } from "express"
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import clientRoutes from "./routes/client/index.route";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// Routes Client
+clientRoutes(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
